2f6d6c6cabfacbae40837c283d49503486e4197d
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-06-11 14:09:36 +0700

    add css extension

--- send_simple_content.php
+++ send_simple_content.php
@@ -8,7 +8,7 @@
   // which is included in the download:
   // https://github.com/sendgrid/sendgrid-php/releases
 
-  $css_code = include('email-style');
+  $css_code = include('email-style.css');
 
   $html_template = '
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
